df00424a72fe2799ba5d2fe1730049aed4e4b132
Mehdi Abasian <110522945+night-coder-13@users.noreply.github.com>
2023-03-15 20:06:46 +0330

    add blog route

--- custom-api/api.php
+++ custom-api/api.php
@@ -8,6 +8,73 @@
  * Author URI: http://watch-learn.com
  */
 
+function wl_catalog() {
+	$args = [
+		'numberposts' => 500,
+		'post_type' => 'catalog'
+	];
+	$posts = get_posts($args);
+
+	$data = [];
+	$i = 0;
+
+	//This is a customized object for the catalog slider (El swiper slider)
+	foreach($posts as $post) {
+		$data[$i]['id'] = $post->ID;
+		$data[$i]['title'] = empty($post->title) ? $post->post_title : $post->title;
+		$data[$i]['slug'] = $post->post_name;
+		$data[$i]['src'] = empty($post->image) ? '' : explode(',',$post->image)[0];
+		$data[$i]['thumb'] = empty($post->image) ? '' : explode(',',$post->image)[0];
+		$data[$i]['subHtml'] = '<div class="lightGallery-captions"><h4>'.$data[$i]['title'].'</h4></div>';
+		$data[$i]['date'] = get_post_datetime( $post->ID, $field = 'modified', $source = 'local' )->format('Y-m-d H:i:s');	
+
+		$i++;
+	}
+        $data = array_reverse($data);
+	return $data;
+}
+function wl_blog($slug = 3) {
+	$args = [
+		'numberposts' => $slug['slug'],
+		'post_type' => 'blog'
+	];
+	$posts = get_posts($args);
+
+	$data = [];
+	$i = 0;
+
+	foreach($posts as $post) {
+	$data[$i]['id'] = $post->ID;
+	$data[$i]['title'] = empty($post->title) ? $post->post_title : $post->title;
+        $data[$i]['slug'] = $post->post_name;
+        $data[$i]['image'] = empty($post->image) ? '' : explode(',',$post->image)[0];
+        $data[$i]['content'] = apply_filters('the_content', $post->post_content); 
+        $data[$i]['author'] = get_the_author_meta('display_name', $post->post_author);
+	$data[$i]['date'] = get_post_datetime( $post->ID, $field = 'modified', $source = 'local' )->format('Y-m-d H:i:s');	
+
+	$i++;
+	}
+
+	return $data;
+}
+ 
+function wl_single_blog($slug) {
+	$args = [
+		'ID' => $slug['slug'],
+		'post_type' => 'blog'
+	];
+	$post = get_post($slug['slug']);
+	$data = [];
+ 	$data['id'] = $post->ID;
+	$data['title'] = empty($post->title) ? $post->post_title : $post->title;
+        $data['slug'] = $post->post_name;
+        $data['image'] = empty($post->image) ? '' : explode(',',$post->image)[0];
+        $data['content'] = apply_filters('the_content', $post->post_content); 
+        $data['author'] = get_the_author_meta('display_name', $post->post_author);
+	$data['date'] = get_post_datetime( $post->ID, $field = 'modified', $source = 'local' )->format('Y-m-d H:i:s');	
+        
+	return $data;
+}
 
 function wl_posts() {
 	$args = [
@@ -68,9 +135,10 @@ function wl_product() {
 
 	foreach($posts as $post) {
 		$data[$i]['id'] = $post->ID;
-		$data[$i]['title'] = $post->title;
+		$data[$i]['title'] = empty($post->title) ? $post->post_title : $post->title;
         $data[$i]['slug'] = $post->post_name;
         $data[$i]['_description'] = $post->_description;
+        
 		if(!empty(explode( ',',$post->image)[0]))
 			$data[$i]['image1'] = wp_get_attachment_image_src( explode( ',',$post->image)[0], 'original' );
 		if(!empty(explode( ',',$post->image)[1]))
@@ -84,13 +152,10 @@ function wl_product() {
 }
 
 function wl_archive($slug) {
-	global $wpdb;      
+        global $wpdb;      
 	$sql = "SELECT * FROM `wp_term_relationships` WHERE `term_taxonomy_id`=".$slug['slug'];
 	$posts = $wpdb->get_results($sql);
-	
-	// A relatively unsuccessful method
-	//$posts=json_decode(file_get_contents('http://localhost/afam-wp/wp-json/wp/v2/product?product_cat='.$slug['slug']));
-			
+		
 	$data = [];
 	$i = 0;
 
@@ -99,9 +164,10 @@ function wl_archive($slug) {
 		$post = get_post($post->object_id);
 
 		$data[$i]['id'] = $post->ID;
-		$data[$i]['title'] = $post->title;
+		$data[$i]['title'] = empty($post->title) ? $post->post_title : $post->title;
         $data[$i]['slug'] = $post->post_name;
         $data[$i]['_description'] = $post->_description;
+        $data[$i]['hashtag'] = $post->hashtag;
 		if(!empty(explode( ',',$post->image)[0]))
         	$data[$i]['image1'] = wp_get_attachment_image_src( explode( ',',$post->image)[0], 'original' );
 		if(!empty(explode( ',',$post->image)[1]))
@@ -109,8 +175,8 @@ function wl_archive($slug) {
 
 		$i++;
 	}
-
-	return $data;
+        return $data;
+	
 }
 // Used in this video https://www.youtube.com/watch?v=76sJL9fd12Y
 function wl_single_product($slug) {
@@ -119,12 +185,12 @@ function wl_single_product($slug) {
 		'post_type' => 'product'
 	];
 	$post = get_post($slug['slug']);
-
 	$data = [];
 		$data['id'] = $post->ID;
-		$data['title'] = $post->title;
+		$data['title'] = empty($post->title) ? $post->post_title : $post->title;
         $data['slug'] = $post->post_name;
         $data['_description'] = $post->_description;
+        $data['hashtag'] = $post->hashtag;
 		if(!empty(explode( ',',$post->image)[0]))
         	$data['image1'] = wp_get_attachment_image_src( explode( ',',$post->image)[0], 'original' );
 		if(!empty(explode( ',',$post->image)[1]))
@@ -150,7 +216,7 @@ function wl_product_cats() {
 		$data[$i]['name'] = $post->name;
 		$thumbnail = get_term_meta($post->term_id)['thumbnail'];
 		if(!empty($thumbnail[0]))
-        	$data[$i]['img'] = wp_get_attachment_image_src( $thumbnail[0], 'original' );
+        	$data[$i]['img'] = wp_get_attachment_image_src( $thumbnail[0], 'thumbnail' );
 
         
         // $data[$i]['price'] = get_field('price', $post->ID);
@@ -198,7 +264,29 @@ add_action('rest_api_init', function() {
 		'methods' => 'GET',
 		'callback' => 'wl_post',
     ) );
+    // catalog
+    register_rest_route('wl/v1', 'catalog', [
+	'methods' => 'GET',
+	'callback' => 'wl_catalog',
+     ]);
+	
     
+    // blog
+    register_rest_route('wl/v1', 'blog', [
+	'methods' => 'GET',
+	'callback' => 'wl_blog',
+    ]);
+
+    register_rest_route('wl/v1', 'blog/(?P<slug>[a-zA-Z0-9-]+)', [
+	'methods' => 'GET',
+	'callback' => 'wl_blog',
+    ]);
+
+    register_rest_route('wl/v1', 'single_blog/(?P<slug>[a-zA-Z0-9-]+)', [
+	'methods' => 'GET',
+	'callback' => 'wl_single_blog',
+    ]);
+
     // product
     register_rest_route('wl/v1', 'product', [
 		'methods' => 'GET',
@@ -225,3 +313,4 @@ add_action('rest_api_init', function() {
 		'callback' => 'wl_archive',
 	]);
 });
+
